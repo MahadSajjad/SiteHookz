0a20c0b69f1a5f92ac6cbbe637e4227bab690a7a
Mahad Sajjad <mahadsajjad787@gmail.com>
2026-08-27 22:48:36 +0500

    feat: implement Prisma client factory with logging and add database seed script

---
+++ packages/database/src/index.d.ts
@@ -0,0 +1,3 @@
+export * from '@prisma/client';
+export declare function createPrismaClient(): any;
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/database/src/index.js
@@ -0,0 +1,8 @@
+export * from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
+export function createPrismaClient() {
+    return new PrismaClient({
+        log: ['query', 'info', 'warn', 'error'],
+    });
+}
+//# sourceMappingURL=index.js.map
